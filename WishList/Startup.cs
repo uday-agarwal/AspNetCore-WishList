ba1755d09f737fb1240a643d2815baf1c5569dba
Uday Agarwal <1411.uday@gmail.com>
2020-04-10 15:01:56 +0530

    Step 2.3

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -25,6 +25,8 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -35,6 +37,7 @@ namespace WishList
             }
 
             app.UseMvcWithDefaultRoute();
+
         }
     }
 }
